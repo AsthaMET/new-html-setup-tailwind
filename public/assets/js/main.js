c5ac8474490078e99b8727cd0b2c6bc155159ea9
AsthaMET <aastham.encircle@gmail.com>
2025-10-18 10:51:17 +0530

    update-assets

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,1 +1,8 @@
-console.log("Hello JS");
+window.addEventListener("scroll", () => {
+  const nav = document.querySelector("nav");
+  if (window.scrollY >= 50) {
+    nav.classList.add("active_nav");
+  } else {
+    nav.classList.remove("active_nav");
+  }
+});
